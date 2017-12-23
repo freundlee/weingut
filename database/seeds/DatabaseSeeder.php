e3c0bc02fc67fd727a3798fe699e2e72cb466737
alan lee <freundlee@gmail.com>
2017-12-23 21:55:27 +0100

    change user to userstableseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         
         $this->command->info('Role table seeded!');
         
+        //
         $this->call('UsersTableSeeder');
         
         $this->command->info('User table seeded!');
