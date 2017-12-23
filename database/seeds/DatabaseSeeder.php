358a6fffee4873ad94e189a147ed134884b82dd6
alan lee <freundlee@gmail.com>
2017-12-23 21:47:27 +0100

    fix bugs of databaseprovider

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         
         $this->command->info('Role table seeded!');
         
-        $this->call('UserTableSeeder');
+        $this->call('UsersTableSeeder');
         
         $this->command->info('User table seeded!');
     }
